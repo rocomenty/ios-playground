f4f05d315af170ed45030942bd9ee52c021296c5
rocomenty <cheng.luo@wustl.edu>
2019-12-08 14:24:47 -0800

    I Am Poor app completed
    yeah i am poor

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Cheng Luo on 12/8/19.
+//  Copyright © 2019 Cheng Luo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
